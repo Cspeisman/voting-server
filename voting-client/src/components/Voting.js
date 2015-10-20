5fd88010fcd77f93cbafa0b9d6418e919bcd4701
Corey Speisman <coreys@hzdg.com>
2015-10-19 22:28:41 -0400

    voting component handles hasvoted and winner props

--- voting-client/src/components/Voting.js
+++ voting-client/src/components/Voting.js
@@ -2,15 +2,32 @@ import React from 'react';
 
 export default class Voting extends React.Component {
 
+  isDisabled() {
+    return !!this.props.hasVoted
+  }
+
+  hasVotedFor(entry) {
+    return this.props.hasVoted === entry;
+  }
+
   render() {
     const entries = this.props.pair.map(entry => {
+
+      const label = this.hasVotedFor(entry) ? <div className='label'>Voted</div> : null;
+
       return (
-        <button key={entry}>
+        <button
+          key={entry}
+          onClick={() => this.props.vote(entry)}
+          disabled={this.isDisabled()}
+        >
           <h1>{entry}</h1>
+          {label}
         </button>
       );
     });
 
-    return <div className='voting'>{entries}</div>;
+    const comp = this.props.winner ? <div ref='winner'>Winner is {this.props.winner}!</div> : entries;
+    return <div className='voting'>{comp}</div>;
   }
 }
